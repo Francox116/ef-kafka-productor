972b4ade36f2a8935d143e4c1e18cd34d851fb4c
Francox116 <francoxde1106@gmail.com>
2025-08-17 18:40:07 -0500

    Creación del proyecto

---
+++ src/main/java/pe/edu/idat/ef_kafka_productor/EfKafkaProductorApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ef_kafka_productor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfKafkaProductorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfKafkaProductorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/ef_kafka_productor/EfKafkaProductorApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ef_kafka_productor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfKafkaProductorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
